93c8c2281edc763de509132c52941d25202c1e80
Shengya Zhang <33270840+PenelopZ@users.noreply.github.com>
2018-02-05 17:21:16 -0600

    Update HW2_3.m

--- HW2_3.m
+++ HW2_3.m
@@ -3,13 +3,6 @@
 % Question 3
 % Shengya Zhang
 
-n=1000;
-U=rand(n,1);
-E=0;
-for j=1:n
-    x=U(j,1);
-    a=(1/((2*pi)^0.5))*(exp(-0.5*(x^2)));
-    E=E+x*a;
-end
 
-E=E+0
+u = normrnd(0,1,[1 100000]);
+E = mean(abs(u))
